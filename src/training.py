c1421bc973c34618b9f9225c798f6ce86f4f555c
Prayas <prayaspatnaik21@gmail.com>
2022-01-30 13:18:46 -0600

    Adding training and DataManagement changes

--- src/training.py
+++ src/training.py
@@ -1,10 +1,12 @@
 import os
-from src.utils.common import read_config
+from utils.common import read_config
+from utils.data_mgmt import get_data
 import argparse
 
 def training(config_path):
     config = read_config(config_path)
-    print(config)
+    validation_datasize = config["params"]["validation_datasize"]
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
 if __name__ == '__main__':
 
